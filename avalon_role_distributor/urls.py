971b6664af4aca3ff02d6416f3796da7f7a4b7b3
seo <cv5006@naver.com>
2025-08-09 16:20:13 +0900

    redirection

--- avalon_role_distributor/urls.py
+++ avalon_role_distributor/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('game/', include('game.urls')),
+    path('', lambda request: redirect('game/'))
 ]
 
 if settings.DEBUG:
