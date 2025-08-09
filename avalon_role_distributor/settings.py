026be7f36c78d453b994f11da9cd711e9151b86f
seo <cv5006@naver.com>
2025-08-09 16:26:39 +0900

    debug true

--- avalon_role_distributor/settings.py
+++ avalon_role_distributor/settings.py
@@ -27,7 +27,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
